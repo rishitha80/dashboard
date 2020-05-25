cfda94a499acae7f35c19fb94a29bde60860dbd0
rishitha80 <48984024+rishitha80@users.noreply.github.com>
2020-05-25 16:34:21 +0530

    Add files via upload

---
+++ Dashboard.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+class Dashboard extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Hii! How are you?</h1>
+            </div>
+        )
+    }
+}
+export default Dashboard;
